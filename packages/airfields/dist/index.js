2cb27d99de7c7f605907b0cae8469ec586edf7c8
fyyyyy <frank.trigub@ext.mercedes-benz.io>
2023-02-10 00:17:35 +0100

    change airport exports to match DCS theater names

--- packages/airfields/dist/index.js
+++ packages/airfields/dist/index.js
@@ -3,11 +3,16 @@ import syria from './syria.json';
 import marianas from './marianas.json';
 import nevada from './nevada.json';
 import persian_gulf from './persian_gulf.json';
+// airports json is an object for some reason
+const caucasusToArray = [];
+Object.values(caucasus).forEach(airport => {
+    caucasusToArray.push(airport);
+});
 const toExport = {
-    caucasus,
-    syria,
-    marianas,
-    nevada,
-    persian_gulf,
+    Caucasus: caucasusToArray,
+    Syria: syria,
+    MarianaIslands: marianas,
+    Nevada: nevada,
+    PersianGulf: persian_gulf,
 };
 export default toExport;
--- packages/airfields/src/index.ts
+++ packages/airfields/src/index.ts
@@ -4,13 +4,18 @@ import marianas from './marianas.json'
 import nevada from './nevada.json'
 import persian_gulf from './persian_gulf.json'
 
+// airports json is an object for some reason
+const caucasusToArray: any[] = []
+Object.values(caucasus).forEach(airport => {
+    caucasusToArray.push(airport);
+});
 
 const toExport: any = {
-    caucasus,
-    syria,
-    marianas,
-    nevada,
-    persian_gulf,
+    Caucasus: caucasusToArray,
+    Syria: syria,
+    MarianaIslands: marianas,
+    Nevada: nevada,
+    PersianGulf: persian_gulf,
 }
 
 export default toExport;
