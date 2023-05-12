623fbeaffde4d0f49fb6bd1e226b43d3a090e0d6
fyyyyy <frankyyyy@live.com>
2023-05-12 12:38:24 +0200

    fix assets dir

--- packages/miz-js/src/demo.ts
+++ packages/miz-js/src/demo.ts
@@ -2,7 +2,7 @@ import { initialize } from "./miz-manager.js";
 import '../lib/ace/ace.js';
 
 const aceEditor = ace.edit("editor");
-ace.config.set("basePath", "https://dcs-web-editor.github.io/dcs-web-editor-mono/lib/ace");
+ace.config.set("basePath", "./lib/ace");
 ace.require("ace/ext-searchbox");
 ace.require("ace/ext-themelist");
 ace.require("ace/ext-modelist");
