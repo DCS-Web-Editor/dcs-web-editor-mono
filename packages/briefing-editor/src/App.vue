035e549fbd85154e32f2f82d9e706d43cd023065
EMcNugget <ethan.d.seys@gmail.com>
2023-05-29 14:12:06 -0500

    Fixed styling and bug fixes for briefing editor

--- packages/briefing-editor/src/App.vue
+++ packages/briefing-editor/src/App.vue
@@ -3,11 +3,11 @@
     :theme="selectedTheme === 'Dark' ? theme : null"
     :theme-overrides="selectedTheme === 'Dark' ? themeOverrides : null"
   >
-    <div class="flex flex-row w-full text-xl mt-2 font-sans font-semibold pr-10">
+    <div class="flex flex-row w-full mt-2 font-sans pr-10">
       <div
         class="flex justify-center flex-col w-full h-full overflow-hidden pl-10"
       >
-        <n-h3 class="border-b border-white border-solid border-1 mb-12">
+        <n-h3 class="border-b border-white border-solid border-1 mb-12 text-xl">
           Briefing
         </n-h3>
         <tasks-sortie />
@@ -15,8 +15,8 @@
       <div
         class="flex justify-center flex-col w-1/2 h-full overflow-hidden pl-8 pr-10 ml-8"
       >
-        <n-h3 class="border-b border-white border-solid border-1 mb-12"
-          >Briefing Images</n-h3
+        <n-h3 class="border-b border-white border-solid border-1 mb-12 text-xl">
+          Briefing Images</n-h3
         >
         <image-upload />
       </div>
@@ -26,6 +26,7 @@
 
 <script setup lang="ts">
 import type { GlobalTheme, GlobalThemeOverrides } from "naive-ui";
+import { NH3 } from "naive-ui";
 import TasksSortie from "./components/TasksSortie.vue";
 import ImageUpload from "./components/ImageUpload.vue";
 import { NConfigProvider } from "naive-ui";
--- packages/briefing-editor/src/components/ImageUpload.vue
+++ packages/briefing-editor/src/components/ImageUpload.vue
@@ -2,9 +2,9 @@
   <n-tabs type="segment">
     <n-tab-pane name="blue" tab="Blue">
       <n-upload
+        :key="componentKey"
         accept=".png, .jpeg, .jpg"
         :default-file-list="previewFileListBlue"
-        :file-list="previewFileListBlue"
         list-type="image-card"
         :custom-request="customRequestBlue"
         :on-remove="onRemove"
@@ -12,9 +12,9 @@
     </n-tab-pane>
     <n-tab-pane name="neutral" tab="Neutral">
       <n-upload
+        :key="componentKey"
         accept=".png, .jpeg, .jpg"
         :default-file-list="previewFileListNeutral"
-        :file-list="previewFileListNeutral"
         list-type="image-card"
         :custom-request="customRequestNeutral"
         :on-remove="onRemove"
@@ -22,9 +22,9 @@
     </n-tab-pane>
     <n-tab-pane name="red" tab="Red">
       <n-upload
+        :key="componentKey"
         accept=".png, .jpeg, .jpg"
         :default-file-list="previewFileListRed"
-        :file-list="previewFileListRed"
         list-type="image-card"
         :custom-request="customRequestRed"
         :on-remove="onRemove"
@@ -47,6 +47,7 @@ import {
   UploadCustomRequestOptions,
 } from "naive-ui";
 import { IBriefingImages } from "../types";
+import { watch, ref } from "vue";
 
 const map = useMapRescStore();
 const img = useImgStore();
@@ -54,6 +55,11 @@ const img_data = useImgDataStore();
 const txt = useTxtState();
 
 let id_num = 6; // image id's start at 6, 1-5 are for descriptions
+const componentKey = ref(0);
+
+const forceRerender = () => {
+  componentKey.value += 1;
+};
 
 const changeId = () => {
   return `ResKey_ImageBriefing_${id_num++}`;
@@ -65,7 +71,7 @@ const changeMaxId = () => {
   } else {
     return id_num;
   }
-}
+};
 
 const placeholder: UploadFileInfo = {
   id: "none",
@@ -98,7 +104,7 @@ const setData = (key: string, name: string, coa: Coalitions) => {
 const findKeys = (keys: string[]): UploadFileInfo[] => {
   return keys.map((key) => {
     const item = img_data.getOneImage(key);
-    if (item !== null) {
+    if (item !== null && item !== undefined) {
       const data: UploadFileInfo = item;
       return {
         id: key,
@@ -154,6 +160,10 @@ const onRemove = (data: {
   img_data.deleteOneImage(data.file.id);
   delete map.map[data.file.id];
   img.briefing = removeIdFromBriefingImages(img.briefing, data.file.id);
+  const event = new CustomEvent("deleteImage", {
+    detail: { id: data.file.id, name: data.file.name },
+  });
+  window.dispatchEvent(event);
   return true;
 };
 
@@ -179,5 +189,26 @@ const previewFileListBlue = computed(() =>
 
 const previewFileListNeutral = computed(() =>
   findKeys(img.briefing.pictureFileNameN)
+);
+
+watch(
+  () => previewFileListNeutral.value,
+  () => {
+    forceRerender();
+  }
+);
+
+watch(
+  () => previewFileListBlue.value,
+  () => {
+    forceRerender();
+  }
+);
+
+watch(
+  () => previewFileListRed.value,
+  () => {
+    forceRerender();
+  }
 );
 </script>
