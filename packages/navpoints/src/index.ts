3e29a3d6c75a6fc2b97961d5e5efbba5c249c419
fyyyy <frankyyyy@live.com>
2024-01-21 21:03:17 +0100

    add navpoints

---
+++ packages/navpoints/src/index.ts
@@ -0,0 +1,20 @@
+import caucasus from './caucasus.json'
+import syria from './syria.json'
+import marianas from './marianas.json'
+import nevada from './nevada.json'
+import sinai from './sinai.json'
+import persian_gulf from './persian_gulf.json'
+import south_atlantic from './south_atlantic.json'
+
+
+const toExport: any = {
+    caucasus,
+    syria,
+    marianas,
+    nevada,
+    sinai,
+    persian_gulf,
+    south_atlantic,
+}
+
+export default toExport;
