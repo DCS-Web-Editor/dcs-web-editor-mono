2dba4eb00fac385fa3b52061bc40d4d54f58198d
fryyyyy <frankyyyy@live.com>
2024-06-06 15:02:49 +0200

    fix kneeboard build

--- packages/kneeboard/vite.config.ts
+++ packages/kneeboard/vite.config.ts
@@ -4,14 +4,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: "",
   build: {
-    assetsDir: 'assets',
+    // assetsDir: 'assets',
     minify: true,
     minifySyntax: true,
-    outDir: 'dist',
+    outDir: "dist",
     cssCodeSplit: false,
-},
+  },
   server: {},
   resolve: {
     alias: {
