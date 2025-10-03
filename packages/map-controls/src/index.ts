40bd8f2dec776dbdad59925766bb628d9031bfa0
fyyyyy <frankyyyy@live.com>
2025-10-03 17:33:49 +0200

    add rulerControl

--- packages/map-controls/src/index.ts
+++ packages/map-controls/src/index.ts
@@ -1,15 +1,16 @@
 export let map: any;
 
 export const context = {
-  iconBar: null,
-  writeMode: false,
-  writeInitialized: false,
-  paintInitialized: false,
-  paintMode: false,
-  searchMode: false,
-  iconMode: false,
-  follow: true,
-  getColor: () => "#0C8",
+    iconBar: null,
+    writeMode: false,
+    writeInitialized: false,
+    paintInitialized: false,
+    paintMode: false,
+    searchMode: false,
+    iconMode: false,
+    follow: true,
+    ruler: false,
+    getColor: () => "#0C8",
 };
 
 export * from "./paintControl";
@@ -19,3 +20,4 @@ export * from "./searchControl";
 export * from "./screenshotControl";
 export * from "./iconControl";
 export * from "./followControl";
+export * from "./rulerControl";
