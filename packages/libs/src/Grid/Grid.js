5be4d5e07aca20a4411cce7a028454f48ac7ff53
fyyyyy <frankyyyy@live.com>
2025-12-02 18:41:55 +0100

    fix grid error

--- packages/libs/src/Grid/Grid.js
+++ packages/libs/src/Grid/Grid.js
@@ -3,240 +3,252 @@
  */
 
 L.Grid = L.LayerGroup.extend({
-	options: {
-		xticks: 8,
-		yticks: 5,
+  options: {
+    xticks: 8,
+    yticks: 5,
 
-		// 'decimal' or one of the templates below
-		coordStyle: 'MinDec',
-		coordTemplates: {
-			'MinDec': '{degAbs}&deg;&nbsp;{minDec}\'{dir}',
-			'DMS': '{degAbs}{dir}{min}\'{sec}"'
-		},
+    // 'decimal' or one of the templates below
+    coordStyle: "MinDec",
+    coordTemplates: {
+      MinDec: "{degAbs}&deg;&nbsp;{minDec}'{dir}",
+      DMS: "{degAbs}{dir}{min}'{sec}\"",
+    },
 
-		// Path style for the grid lines
-		lineStyle: {
-			stroke: true,
-			color: '#08f',
-			opacity: 0.3,
-			weight: 1
-		},
-		
-		// Redraw on move or moveend
-		redraw: 'move'
-	},
+    // Path style for the grid lines
+    lineStyle: {
+      stroke: true,
+      color: "#08f",
+      opacity: 0.3,
+      weight: 1,
+    },
 
-	initialize: function (options) {
-		L.LayerGroup.prototype.initialize.call(this);
-		L.Util.setOptions(this, options);
+    // Redraw on move or moveend
+    redraw: "move",
+  },
 
-	},
+  initialize: function (options) {
+    L.LayerGroup.prototype.initialize.call(this);
+    L.Util.setOptions(this, options);
+  },
 
-	onAdd: function (map) {
-		this._map = map;
+  onAdd: function (map) {
+    this._map = map;
 
-		var grid = this.redraw();
-		this._map.on('viewreset '+ this.options.redraw, function () {
-			grid.redraw();
-		});
+    var grid = this.redraw();
+    this._map.on("viewreset " + this.options.redraw, function () {
+      grid.redraw();
+    });
 
-		this.eachLayer(map.addLayer, map);
-	},
-	
-	onRemove: function (map) {
-		// remove layer listeners and elements
-		map.off('viewreset '+ this.options.redraw, this.map);
-		this.eachLayer(this.removeLayer, this);
-	},
+    this.eachLayer(map.addLayer, map);
+  },
 
-	redraw: function () {
-		if (!this._map) return;
-		// pad the bounds to make sure we draw the lines a little longer
-		this._bounds = this._map.getBounds().pad(0.5);
+  onRemove: function (map) {
+    // remove layer listeners and elements
+    map.off("viewreset " + this.options.redraw, this.map);
+    this.eachLayer(this.removeLayer, this);
+  },
 
-		var grid = [];
-		var i;
+  redraw: function () {
+    if (!this._map) return;
+    // pad the bounds to make sure we draw the lines a little longer
+    this._bounds = this._map.getBounds().pad(0.5);
 
-		var latLines = this._latLines();
-		for (i in latLines) {
-			if (Math.abs(latLines[i]) > 90) {
-				continue;
-			}
-			grid.push(this._horizontalLine(latLines[i]));
-			grid.push(this._label('lat', latLines[i]));
-		}
+    var grid = [];
+    var i;
 
-		var lngLines = this._lngLines();
-		for (i in lngLines) {
-			grid.push(this._verticalLine(lngLines[i]));
-			grid.push(this._label('lng', lngLines[i]));
-		}
+    var latLines = this._latLines();
+    for (i in latLines) {
+      if (Math.abs(latLines[i]) > 90) {
+        continue;
+      }
+      grid.push(this._horizontalLine(latLines[i]));
+      grid.push(this._label("lat", latLines[i]));
+    }
 
-		this.eachLayer(this.removeLayer, this);
+    var lngLines = this._lngLines();
+    for (i in lngLines) {
+      grid.push(this._verticalLine(lngLines[i]));
+      grid.push(this._label("lng", lngLines[i]));
+    }
 
-		for (i in grid) {
-			this.addLayer(grid[i]);
-		}
-		return this;
-	},
+    this.eachLayer(this.removeLayer, this);
 
-	_latLines: function () {
-		return this._lines(
-			this._bounds.getSouth(),
-			this._bounds.getNorth(),
-			this.options.yticks * 2,
-			this._containsEquator()
-		);
-	},
-	_lngLines: function () {
-		return this._lines(
-			this._bounds.getWest(),
-			this._bounds.getEast(),
-			this.options.xticks * 2,
-			this._containsIRM()
-		);
-	},
+    for (i in grid) {
+      this.addLayer(grid[i]);
+    }
+    return this;
+  },
 
-	_lines: function (low, high, ticks, containsZero) {
-		var delta = low - high,
-			tick = this._round(delta / ticks, delta);
+  _latLines: function () {
+    return this._lines(
+      this._bounds.getSouth(),
+      this._bounds.getNorth(),
+      this.options.yticks * 2,
+      this._containsEquator()
+    );
+  },
+  _lngLines: function () {
+    return this._lines(
+      this._bounds.getWest(),
+      this._bounds.getEast(),
+      this.options.xticks * 2,
+      this._containsIRM()
+    );
+  },
 
-		if (containsZero) {
-			low = Math.floor(low / tick) * tick;
-		} else {
-			low = this._snap(low, tick);
-		}
+  _lines: function (low, high, ticks, containsZero) {
+    var delta = low - high,
+      tick = this._round(delta / ticks, delta);
 
-		var lines = [];
-		for (var i = -1; i <= ticks; i++) {
-			lines.push(low - (i * tick));
-		}
-		return lines;
-	},
+    if (containsZero) {
+      low = Math.floor(low / tick) * tick;
+    } else {
+      low = this._snap(low, tick);
+    }
 
-	_containsEquator: function () {
-		var bounds = this._map.getBounds();
-		return bounds.getSouth() < 0 && bounds.getNorth() > 0;
-	},
+    var lines = [];
+    for (var i = -1; i <= ticks; i++) {
+      lines.push(low - i * tick);
+    }
+    return lines;
+  },
 
-	_containsIRM: function () {
-		var bounds = this._map.getBounds();
-		return bounds.getWest() < 0 && bounds.getEast() > 0;
-	},
+  _containsEquator: function () {
+    var bounds = this._map.getBounds();
+    return bounds.getSouth() < 0 && bounds.getNorth() > 0;
+  },
 
-	_verticalLine: function (lng) {
-		return new L.Polyline([
-			[this._bounds.getNorth(), lng],
-			[this._bounds.getSouth(), lng]
-		], this.options.lineStyle);
-	},
-	_horizontalLine: function (lat) {
-		return new L.Polyline([
-			[lat, this._bounds.getWest()],
-			[lat, this._bounds.getEast()]
-		], this.options.lineStyle);
-	},
+  _containsIRM: function () {
+    var bounds = this._map.getBounds();
+    return bounds.getWest() < 0 && bounds.getEast() > 0;
+  },
 
-	_snap: function (num, gridSize) {
-		return Math.floor(num / gridSize) * gridSize;
-	},
+  _verticalLine: function (lng) {
+    lng ||= 0;
+    return new L.Polyline(
+      [
+        [this._bounds.getNorth(), lng],
+        [this._bounds.getSouth(), lng],
+      ],
+      this.options.lineStyle
+    );
+  },
+  _horizontalLine: function (lat = 0) {
+    lat ||= 0;
+    return new L.Polyline(
+      [
+        [lat, this._bounds.getWest()],
+        [lat, this._bounds.getEast()],
+      ],
+      this.options.lineStyle
+    );
+  },
 
-	_round: function (num, delta) {
-		var ret;
+  _snap: function (num, gridSize) {
+    return Math.floor(num / gridSize) * gridSize;
+  },
 
-		delta = Math.abs(delta);
-		if (delta >= 1) {
-			if (Math.abs(num) > 1) {
-				ret = Math.round(num);
-			} else {
-				ret = (num < 0) ? Math.floor(num) : Math.ceil(num);
-			}
-		} else {
-			var dms = this._dec2dms(delta);
-			if (dms.min >= 1) {
-				ret = Math.ceil(dms.min) * 60;
-			} else {
-				ret = Math.ceil(dms.minDec * 60);
-			}
-		}
+  _round: function (num, delta) {
+    var ret;
 
-		return ret;
-	},
+    delta = Math.abs(delta);
+    if (delta >= 1) {
+      if (Math.abs(num) > 1) {
+        ret = Math.round(num);
+      } else {
+        ret = num < 0 ? Math.floor(num) : Math.ceil(num);
+      }
+    } else {
+      var dms = this._dec2dms(delta);
+      if (dms.min >= 1) {
+        ret = Math.ceil(dms.min) * 60;
+      } else {
+        ret = Math.ceil(dms.minDec * 60);
+      }
+    }
 
-	_label: function (axis, num) {
-		var latlng;
-		var bounds = this._map.getBounds().pad(-0.005);
+    return ret;
+  },
 
-		if (axis == 'lng') {
-			latlng = L.latLng(bounds.getNorth(), num);
-		} else {
-			latlng = L.latLng(num, bounds.getWest());
-		}
+  _label: function (axis, num) {
+    num ||= 0;
+    var latlng;
+    var bounds = this._map.getBounds().pad(-0.005);
 
-		return L.marker(latlng, {
-			icon: L.divIcon({
-				iconSize: [0, 0],
-				className: 'leaflet-grid-label',
-				html: '<div class="' + axis + '" tabindex="-1">' + this.formatCoord(num, axis) + '</div>'
-			}),
-      keyboard: false
-		});
-	},
+    if (axis == "lng") {
+      latlng = L.latLng(bounds.getNorth(), num);
+    } else {
+      latlng = L.latLng(num, bounds.getWest());
+    }
 
-	_dec2dms: function (num) {
-		var deg = Math.floor(num);
-		var min = ((num - deg) * 60);
-		var sec = Math.floor((min - Math.floor(min)) * 60);
-		return {
-			deg: deg,
-			degAbs: Math.abs(deg),
-			min: Math.floor(min),
-			minDec: min,
-			sec: sec
-		};
-	},
+    return L.marker(latlng, {
+      icon: L.divIcon({
+        iconSize: [0, 0],
+        className: "leaflet-grid-label",
+        html:
+          '<div class="' +
+          axis +
+          '" tabindex="-1">' +
+          this.formatCoord(num, axis) +
+          "</div>",
+      }),
+      keyboard: false,
+    });
+  },
 
-	formatCoord: function (num, axis, style) {
-		if (!style) {
-			style = this.options.coordStyle;
-		}
-		if (style == 'decimal') {
-			var digits;
-			if (num >= 10) {
-				digits = 2;
-			} else if (num >= 1) {
-				digits = 3;
-			} else {
-				digits = 4;
-			}
-			return num.toFixed(digits);
-		} else {
-			// Calculate some values to allow flexible templating
-			var dms = this._dec2dms(num);
+  _dec2dms: function (num) {
+    var deg = Math.floor(num);
+    var min = (num - deg) * 60;
+    var sec = Math.floor((min - Math.floor(min)) * 60);
+    return {
+      deg: deg,
+      degAbs: Math.abs(deg),
+      min: Math.floor(min),
+      minDec: min,
+      sec: sec,
+    };
+  },
 
-			var dir;
-			if (dms.deg === 0) {
-				dir = '&nbsp;';
-			} else {
-				if (axis == 'lat') {
-					dir = (dms.deg > 0 ? 'N' : 'S');
-				} else {
-					dir = (dms.deg > 0 ? 'E' : 'W');
-				}
-			}
+  formatCoord: function (num, axis, style) {
+    if (!style) {
+      style = this.options.coordStyle;
+    }
+    if (style == "decimal") {
+      var digits;
+      if (num >= 10) {
+        digits = 2;
+      } else if (num >= 1) {
+        digits = 3;
+      } else {
+        digits = 4;
+      }
+      return num.toFixed(digits);
+    } else {
+      // Calculate some values to allow flexible templating
+      var dms = this._dec2dms(num);
 
-			return L.Util.template(
-				this.options.coordTemplates[style],
-				L.Util.extend(dms, {
-					dir: dir,
-					minDec: Math.round(dms.minDec, 2)
-				})
-			);
-		}
-	}
+      var dir;
+      if (dms.deg === 0) {
+        dir = "&nbsp;";
+      } else {
+        if (axis == "lat") {
+          dir = dms.deg > 0 ? "N" : "S";
+        } else {
+          dir = dms.deg > 0 ? "E" : "W";
+        }
+      }
 
+      return L.Util.template(
+        this.options.coordTemplates[style],
+        L.Util.extend(dms, {
+          dir: dir,
+          minDec: Math.round(dms.minDec, 2),
+        })
+      );
+    }
+  },
 });
 
 L.grid = function (options) {
-	return new L.Grid(options);
+  return new L.Grid(options);
 };
